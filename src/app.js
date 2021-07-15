d6e003cf841ca54c2271faf11f61cc30d456f09c
Chris Carey <cpc9982@nyu.edu>
2021-07-15 10:29:19 -0400

    Rename shader variables for clarity. Move some shader accumulation operations to functions.

--- src/app.js
+++ src/app.js
@@ -64,7 +64,7 @@ function render(geoJsonData) {
       datum.center = getPolygonCenter(datum.blob);
     }
 
-    drawClippingSpheres(viewer, geoJsonData);
+    drawClippingSpheres(viewer, geoJsonData, Dimension.GENDER);
     drawer.shadeEnvironment(e.pointcloud);
   });
 };
